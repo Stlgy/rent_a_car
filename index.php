9503b1b7bf4e9bbba23e274be2b9fa5b07c4e6a5
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-08-14 21:53:29 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+require_once('includes/classes.php');
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Stigy's Car Rental</title>
+    <meta charset="UTF-8">
+    <meta name="author" content =" ">
+    <meta name="description" content=" ">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <script type=text/javascript src="js/jquery.js"></script>
+    <script type=text/javascript src="js/jmain.js"></script>
+</head>
+<body>
+    <section class="">
+        <?php 
+            include 'includes/header.php';
+        ?>
+</body>
+</html>
